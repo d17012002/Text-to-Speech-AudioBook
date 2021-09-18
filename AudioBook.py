a71e251d313f9e1cabf2027345aef40fe1841a18
Anurag Kumar <81901470+d17012002@users.noreply.github.com>
2021-09-18 13:49:12 +0530

    Add files via upload

---
+++ AudioBook.py
@@ -0,0 +1,12 @@
+#download your book as txt file and then save it as "MyBook" along with your codes then enjoy!
+import pyttsx3 as py
+speaker=py.init()
+voices=speaker.getProperty('voices')
+speaker.setProperty('voice', voices[0].id)
+speaker.setProperty('rate',150)
+a=open("MyBook.txt")
+while(a.readline()!=None):
+     text=a.readline()
+     print(text)
+     speaker.say(text)
+     speaker.runAndWait()
